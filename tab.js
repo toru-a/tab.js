c1e8be94b045ed633a416c14ca737c7ba61c8bf1
toru-a <148777061+toru-a@users.noreply.github.com>
2023-11-27 21:57:28 +0900

    finished

--- tab.js
+++ tab.js
@@ -6,23 +6,23 @@
   const $content = $tab.querySelectorAll('[data-content]');
   const ACTIVE_CLASS = 'is-active';
   const navLen = $nav.length;
-  
-  //初期化
+
   const init = () => {
     $content[0].style.display = 'block';
     $nav[0].classList.add(ACTIVE_CLASS);
   };
   init();
 
-  //クリックしたら起こるイベント
+  
   const handleClick = (e) => {
+    // リンクタグのためつける
     e.preventDefault();
 
-    //クリックされたnavとそのdataを取得
+  
     const $this = e.target;
     const targetVal = $this.dataset.nav;
 
-    //対象外のnav, content全て一旦リセットする
+   
     let index = 0;
     while(index < navLen){
       $content[index].style.display = 'none';
@@ -30,12 +30,11 @@
       index++;
     }
 
-    //対象のコンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
     $nav[targetVal].classList.add(ACTIVE_CLASS);
   };
 
-  //全nav要素に対して関数を適応・発火
+  
   let index = 0;
   while(index < navLen){
     $nav[index].addEventListener('click', (e) => handleClick(e));  
